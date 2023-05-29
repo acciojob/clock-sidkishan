e5c78ed0530772be2ba17347a56627cb119cdc30
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-30 00:48:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let p = document.getElementById("timer");
+let d = new Date();
+let c = d.getDay();
+let a = d.getDate();
+let b = d.getFullYear();
+
+let k = d.getHours();
+let q = d.getMinutes();
+ let h = setInterval(() => {
+    let z = d.getSeconds();
+}, 1000);
+let s = h;
+console.log(p.innerText = c+"/"+a+"/"+b+", "+k+":"+q+":"+s+" Am");
